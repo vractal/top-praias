b1386ea93ed08331994c5d75c4164d724feabfcc
debmias <debmias@debian>
2020-01-29 19:47:30 -0300

    Add fetchBeaches cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,26 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+const cors = require('cors')({
+  origin: true,
+});
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+exports.fetchBeaches = functions.https.onRequest((request, response) => {
+
+  cors(request, response, () => {
+
+    const docs = admin.firestore().collection("beaches").get().then(snapshot => {
+      beaches = {}
+      snapshot.forEach(doc => {
+        id = doc.id
+        beaches[id] = doc.data()
+      })
+      response.status(200).send({data: beaches});
+      return ""
+    }).catch(exception => {
+      response.status(200).send({error: exception})
+    })
+
+  })
+})
