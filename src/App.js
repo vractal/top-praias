8ac69e9d4fdc0bfa229ca59ca2394bf34bac148e
debmias <debmias@debian>
2020-01-29 22:47:25 -0300

    Add name and state filtering

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 import BeachesList from './beaches/List'
 import './App.css';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
 
 function App() {
   const [beaches, setBeaches] = useState([])
-
+  const [filters, setFilters] = useState({ name: undefined, state: undefined })
   useEffect(() => {
     const url = "https://us-central1-top-praias-29185.cloudfunctions.net/fetchBeaches"
     axios.get(url).then(response => setBeaches(response.data.data))
@@ -13,7 +15,9 @@ function App() {
 
   return (
     <div className="App">
-      <BeachesList list={beaches} />
+      <TextField  style={{ paddingTop: 20, width: '90%'}}  placeholder="Filtrar por nome ou cidade" onChange={event => setFilters({...filters, name: event.target.value})}/>
+      <TextField  style={{ paddingTop: 20, width: '90%'}} placeholder="Filtrar por estado" onChange={event => setFilters({...filters, state: event.target.value})}/>
+      <BeachesList list={beaches} filters={filters}/>
     </div>
   );
 }
--- src/beaches/List.js
+++ src/beaches/List.js
@@ -1,16 +1,36 @@
 import React from 'react'
 import map from 'lodash/map'
+import Beach from './Beach'
 
+export default ({ list, filters }) => (
+  <div>
+    {list && map(list,beach => {
+      const show = ({state,location, name }) => {
+        let show = true
 
-export default ({ list }) => {
-  return <div>{map(list, ({ name, location, imgSrc }) => {
-    return (
-      <div>
-        <h1>{name}</h1>
-        <img src={imgSrc} />
-        <h3>{location}</h3>
-      </div>
-    )
-  })}
+        const normalizeString = str => {
+          str = str.toUpperCase()
+          str = str.replace(/[ÀÁÂÃÄÅ]/,"A");
+          str = str.replace(/[ÈÉÊË]/,"E");
+          str = str.replace(/[Ç]/,"C");
+          str = str.replace(/[Í]/,"I");
+          str = str.replace(/[ÓÔ]/,"O");
+
+          return str.replace(/[^A-Z0-9]/gi,'');
+        }
+
+        if (filters.state){
+          show = normalizeString(state).includes(normalizeString(filters.state))
+        }
+        if (filters.name) {
+          show = normalizeString(name).includes(normalizeString(filters.name)) || normalizeString(location).includes(normalizeString(filters.name))
+        }
+        return show
+      }
+
+      return show(beach) && <Beach {...beach} />
+
+    })}
   </div>
-}
+)
+  
