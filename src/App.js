c57421d9643aa25f7c756aafade0b58dc30ca588
debmias <debmias@debian>
2020-01-31 00:09:17 -0300

    Allow simultaneous filters and improve performance

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
-
+import map from 'lodash/map'
 import './App.css';
 import BeachesList from './beaches/List'
 import Menu from './menu/Index'
-
+import { normalizeString } from 'utils'
 
 function App() {
   const [beaches, setBeaches] = useState([])
@@ -12,7 +12,10 @@ function App() {
 
   useEffect(() => {
     const url = "https://us-central1-top-praias-29185.cloudfunctions.net/fetchBeaches"
-    axios.get(url).then(response => setBeaches(response.data.data))
+    axios.get(url).then(response => {
+      const normalizedList = map(response.data.data, beach => ({...beach, normalizedState: normalizeString(beach.state), normalizedName: normalizeString(beach.name), normalizedLocation: normalizeString(beach.location)}))
+      setBeaches(normalizedList)
+    })
   },[])
 
   return (
--- src/beaches/List.js
+++ src/beaches/List.js
@@ -1,31 +1,20 @@
 import React from 'react'
 import map from 'lodash/map'
 import Beach from './Item'
+import { normalizeString } from '../utils'
 
 export default ({ list, filters }) => (
   <div>
     {list && map(list,beach => {
-      const show = ({state,location, name }) => {
-        let show = true
-
-        const normalizeString = str => {
-          str = str.toUpperCase()
-          str = str.replace(/[ÀÁÂÃÄÅ]/,"A");
-          str = str.replace(/[ÈÉÊË]/,"E");
-          str = str.replace(/[Ç]/,"C");
-          str = str.replace(/[Í]/,"I");
-          str = str.replace(/[ÓÔ]/,"O");
-
-          return str.replace(/[^A-Z0-9]/gi,'');
+      const show = ({normalizedState, normalizedName, normalizedLocation }) => {
+        if (!filters.state && !filters.name){
+          return true
         }
 
-        if (filters.state){
-          show = normalizeString(state).includes(normalizeString(filters.state))
-        }
-        if (filters.name) {
-          show = normalizeString(name).includes(normalizeString(filters.name)) || normalizeString(location).includes(normalizeString(filters.name))
-        }
-        return show
+        const stateMatch = normalizedState.includes(normalizeString(filters.state))
+        const nameMatch = normalizedName.includes(normalizeString(filters.name)) || normalizedLocation.includes(normalizeString(filters.name))
+
+        return stateMatch && nameMatch
       }
 
       return show(beach) && <Beach {...beach} />
