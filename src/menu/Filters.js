25fd923fae811d3039ba980d49ab70996f465367
debmias <debmias@debian>
2020-01-30 23:39:13 -0300

    Change state filter component

--- src/menu/Filters.js
+++ src/menu/Filters.js
@@ -1,11 +1,25 @@
-import React from 'react'
-import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
+import React, { useState } from 'react'
+import Select from 'react-select'
+import uniqBy from 'lodash/uniqBy'
+import uniq from 'lodash/uniq'
+import map from 'lodash/map'
 
-export default ({ filters, setFilters }) => {
+
+export default ({ beaches, filters, setFilters }) => {
+  const [selectedState, setSelectedState ] = useState(undefined)
+  const states = uniqBy(map(beaches,({ state }) => ({ value: state, label: state})),'value')
+
   return <div className="filters">
   <input placeholder="Filtrar por nome ou cidade" onChange={event => setFilters({...filters, name: event.target.value})}/>
-  <input placeholder="Filtrar por estado" onChange={event => setFilters({...filters, state: event.target.value})}/>
+  <Select
+   className="select"
+   placeholder="Filtrar por estado"
+   isClearable
+   options={states}
+   inputValue={selectedState}
+   onInputChange={(option) => setSelectedState(option)}
+   onChange={(option) => setFilters({...filters, state:  option && option.value})}
+   />
   <p> Use os campos acima para filtrar a lista por nome, cidade ou Estado. Foque as imagens para ver a descrição.</p>
   </div>
 }
