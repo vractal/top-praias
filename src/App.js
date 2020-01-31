e9617bcd4181322bfda1450f6491309daa081e80
debmias <debmias@debian>
2020-01-31 00:29:51 -0300

    Handle loading/error on fetch

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import map from 'lodash/map'
 import './App.css';
 import BeachesList from './beaches/List'
 import Menu from './menu/Index'
-import { normalizeString } from 'utils'
+import { normalizeString } from './utils'
+import loadingGif from './loading.gif'
 
 function App() {
-  const [beaches, setBeaches] = useState([])
+  const [beaches, setBeaches] = useState(null)
+  const [error, setError] = useState(null)
   const [filters, setFilters] = useState({ name: undefined, state: undefined })
 
   useEffect(() => {
@@ -15,13 +17,21 @@ function App() {
     axios.get(url).then(response => {
       const normalizedList = map(response.data.data, beach => ({...beach, normalizedState: normalizeString(beach.state), normalizedName: normalizeString(beach.name), normalizedLocation: normalizeString(beach.location)}))
       setBeaches(normalizedList)
+    }).catch(error => {
+      console.log('Error:',error)
+      setError("Algo de errado aconteceu!")
     })
   },[])
 
   return (
     <div className="app">
-          <Menu beaches={beaches} filters={filters} setFilters={setFilters} />
-          {beaches &&  <BeachesList  list={beaches} filters={filters}/>}
+          {beaches ? (
+            <>
+              <Menu beaches={beaches} filters={filters} setFilters={setFilters} />
+              <BeachesList  list={beaches} filters={filters}/>
+            </>
+        ) : <img alt="loading" src={loadingGif} /> }
+        {error && <h1>{error}</h1>}
    </div>
   )
 }
